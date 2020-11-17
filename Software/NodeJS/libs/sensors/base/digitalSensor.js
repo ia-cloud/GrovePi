8c67c55788e3eb5891ece5c6311132f11b8cee9d
hashisan-atBridge <consulting@hashimukai.com>
2020-11-17 16:02:46 +0900

    Update digitalSensor.js
    maybe, depending on the version of grovepi firmware, original repo's commit #452
    * Correct bug on DigitalInput Sensor read. Read 2 bytes instead of one to get the digital value. Don't know own it could work before... Tested with GrovePi firmware 1.3.0.
    dosen't work on older firmware.
    added checking code for either i2c i/f.

--- Software/NodeJS/libs/sensors/base/digitalSensor.js
+++ Software/NodeJS/libs/sensors/base/digitalSensor.js
@@ -13,7 +13,11 @@ DigitalSensor.prototype.read = function() {
   var writeRet = this.board.writeBytes(commands.dRead.concat([this.pin, commands.unused, commands.unused]))
   if (writeRet) {
     this.board.wait(100)
-    return this.board.readBytes(2)[1]
+    // on old version, this.board.readBytes(2)[1] returns 255 and this.board.readBytes()[0] returns the buttom value
+    // newer? version, this.board.readBytes()[0] returns 1 and this.board.readBytes(2)[1] returns the buttom value
+    let v = this.board.readBytes(2)[1]
+    if (v !== 0 && v !== 1) return this.board.readBytes()[0]
+    else return v
   } else {
     return false
   }
